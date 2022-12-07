47617a4e7ac04685e9d8ae78eeabd468e063065d
OVGI <girichevao@gmail.com>
2022-12-07 23:53:23 +0300

    init commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿int a = 0;
+int b = a;
+
+/*Console.Write($"Введите первое число: ");
+int.TryParse(Console.ReadLine()!, out a);
+Console.Write($"Введите второе число: ");
+int.TryParse(Console.ReadLine()!, out b);*/
+
+int index = 0;
+
+while (index < 5)
+{
+    Console.Write($"{index} ");
+    index++;
+}
+
+Console.Write($"\n");
+for (int index2 = 0; index2 < 5; index2++)
+{
+    Console.Write($"{index2} ");
+}
+
